f1c5d4c2e7848b012dbeb7c653c3f09f22859ae9
imranimo <imranesn125@gmail.com>
2017-12-25 11:15:28 +0600

    data

--- data/db.php
+++ data/db.php
@@ -1,11 +1,21 @@
 <?php
 
-	$host="127.0.0.1:3306";
+	$host="127.0.0.1";
 	$user="root";
 	$pass="";
-	$db="prototype";
+	$db="ecommerce";
+    $port=3306;
 	
-	$conn=mysqli_connect($host,$user,$pass,$db);
+	//$conn=mysqli_connect($host,$user,$pass,$db);
+    function executeSQL($sql){
+        global $host, $user, $pass, $dbname, $port;
+        
+        $link=mysqli_connect($host, $user, $pass, $dbname, $port);
+        $result = mysqli_query($link, $sql);
+        mysqli_close($link);
+        
+        return $result;
+    }
 
  
 ?>
