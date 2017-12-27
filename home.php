68b345a7bb64bb48c3a3f420f868c50ad40cef83
uddinminhaz <uddinminhaz09@gmail.com>
2017-12-27 21:33:44 +0600

    home

---
+++ home.php
@@ -0,0 +1,216 @@
+<html>
+
+<head><title>Home</title></head>
+
+	<table align="center" width="1200" >
+	
+			<tr>
+         <th colspan="3" align="center" width="60%">
+            <img src="app/res/common/ali.png" align="left" align="top" width="20%">
+            <br>
+            <br>
+
+           
+                <input type="text" name="search" placeholder="Enter keyword Here....">
+                <input type="submit" value="Search Here">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="app/account/login.php">Login</a>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            
+                <a href="app/account/login.php">Order</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="app/user_pages/cart.php">Cart</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br><br>
+
+    </tr>
+		<tr>
+		<td  valign="top"  width="10%">
+		
+		
+		
+		<label><b>Catagories</b></label><br><hr>
+			<ul>
+			<a href="app/visitor/select persons male.php">Men's Product</a><hr>
+			<li><a href="app/visitor/spm products.php?pname=Shirt">Men's Shirts</a></li>
+			<li><a href="app/visitor/spm products.php?pname=Pant">Men's Pants</a></li>
+			<li><a href="app/visitor/spm products.php?pname=Shoe">Men's Shoes</a></li>
+			<li><a href="app/visitor/spm products.php?pname=Belt">Belt</a></li><br>
+			<a href="app/visitor/select persons female.php">Women's Product</a><hr>
+			<li><a href="app/visitor/spf products.php?pname=Dress">Dresses</a></li>
+			<li><a href="app/visitor/spf products.php?pname=Pant">Pants</a></li>
+			<li><a href="app/visitor/spf products.php?pname=Shoe">Shoes</a></li>
+			<li><a href="app/visitor/spf products.php?pname=Bags">Bags</a></li><br>
+			<a href="app/visitor/select persons kids.php">Kid's Product</a><hr>
+			<li><a href="app/visitor/spk products.php?pname=Dress">Dress</a></li>
+			<li><a href="app/visitor/spk products.php?pname=Diapers">Diapars</a></li>
+			<li><a href="app/visitor/spk products.php?pname=Shoe">Shoes</a></li>
+			<li><a href="app/visitor/spk products.php?pname=Toys">Toys</a></li><br>
+			<a href="app/visitor/accessories.php">Accessories</a><hr>
+			<li><a href="app/visitor/ack products.php?pname=Wallet">Wallet</a></li>
+			<li><a href="app/visitor/ack products.php?pname=Bag">Bags</a></li>
+			<li><a href="app/visitor/ack products.php?pname=BackCover">Backcovers</a></li>
+			<li><a href="app/visitor/ack products.php?pname=HandBand">Handsbands</a></li><br>
+			<a href="app/visitor/electronics.php">Electronics</a><hr>
+			<li><a href="app/visitor/eck products.php?pname=Mobile">Mobile</a></li>
+			<li><a href="app/visitor/eck products.php?pname=TableFan">Table Fans</a></li>
+			<li><a href="app/visitor/eck products.php?pname=HeadPhone">Headphones</a></li>
+			<li><a href="app/visitor/eck products.php?pname=Smart Watch">Smart Watches</a></li><br>
+			</ul>
+		</td>
+		<td></td>
+			
+			<td valign="top" width="40%" >
+					<img src="app/res/products/home.jpg" align="left" align="top" width="100%" >
+					
+					<table align="left" width="100%" >
+					<tr>
+					<tr><td><br></td></tr>
+						<tr>
+
+						<td>
+							<img src="app/res/products/images.jpg"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp; <a href="select persons male.php"> Men's Item </a> </h3>
+						</td>
+						<td>
+							<img src="app/res/products/female.png"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="select persons female.php"> Female's Item </a> </h3>
+						</td>
+							<td>
+							<img src="app/res/products/cutmypic.png"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="select persons kids.php"> kids's Item </a></h3>
+						</td>
+							<td>
+							<img src="app/res/products/accessories.png"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="accessories.php"> Accessories </a></h3>
+						</td>
+							<td>
+							<img src="app/res/products/electronics.png"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="electronics.php"> Electromics</a></h3>
+
+						</td>
+						
+						</tr>
+
+						<tr>
+						<td>
+
+							<br><br>
+							
+							</td>
+						</tr>
+
+
+						<tr>
+
+						<br>
+						<br>
+							<td><h2><font color="red" >Sales Upto 80%</font></h2></td>
+						</tr>
+						<tr>
+						<td>
+
+							<br>
+							
+							</td>
+							</tr>
+
+						<tr>
+						<td>
+							<img src="app/res/products/bag.jpg"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Shoulder bag</a></h3>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<font color="red" >Discount 80%</font></h3>
+
+						</td>
+
+						<td>
+							<img src="app/res/products/shoe.jpg"  width="150"><br>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Casual shoe</a></h3>
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<font color="red" >Discount 40%</font></h3>
+
+						</td>
+
+						</tr>
+
+						<tr>
+						<td>
+
+							<br><br>
+							
+							</td>
+						</tr>
+
+						<tr>
+
+						<br>
+						<br>
+							<td width="300"><h2><font color="red" >Top Selling Products</font></h2></td>
+						</tr>
+						<tr>
+						<td width="300">
+
+							<br>
+							
+							</td>
+
+</tr>
+
+                       <tr>
+						<td width="250">
+							<img src="app/res/products/pic.jpg"  width="150">
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Printed Tops</a></h3>
+							
+
+						</td>
+
+						<td width="250">
+							<img src="app/res/products/watch.jpg"  width="150">
+							<h3><a href="">Fashionable watch</a></h3>
+						</td>
+
+						
+
+						<td width="250">
+							<img src="app/res/products/lipstic.jpg"  width="150">
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Lipstic Set</a></h3>
+						</td>
+
+						
+
+						<td width="250">
+							<img src="app/res/products/hijab.jpg"  width="150">
+							<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Hijab</a></h3>
+						</td>
+
+
+						</tr>
+
+						<tr>
+						<td>
+
+							<br><br>
+							
+							</td>
+						</tr>
+
+						</tr>
+
+
+					
+			</td>
+			
+	</tr>
+			
+			<tr height="100">
+				<th colspan="4">
+					<a href="app/visitor/aboutus.php">About Us</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="app/visitor/learnmore.php">Learn more</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="app/visitor/help.php">Help</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="app/visitor/license.php">Liscence</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				</th>
+			</tr>	
+		
+		
+	
+	
+	
+	
+	</table>
+
+
+</html>
